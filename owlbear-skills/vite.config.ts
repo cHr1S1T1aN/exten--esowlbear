cd11094b982cf4266e376ae71fb0bf35ddd98724
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-12-18 23:55:01 -0300

    bug fix: add edit button to skill items

--- owlbear-skills/vite.config.ts
+++ owlbear-skills/vite.config.ts
@@ -2,13 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    host: true,
-    port: 5173,
     cors: true,
-    allowedHosts: [".ngrok-free.dev"],
-  },
-
-  preview: {
-    port: 4173,
+    allowedHosts: true,
   },
 });
